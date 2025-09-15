a75e5971a50e7f8bf7bc1cb0cdc427d2edad813b
znach <znachok.egor@gmail.com>
2025-09-15 19:27:36 +0300

    init

---
+++ src/main/java/org/example/blog_platform/BlogPlatformApplication.java
@@ -0,0 +1,12 @@
+package org.example.blog_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogPlatformApplication.class, args);
+    }
+}
