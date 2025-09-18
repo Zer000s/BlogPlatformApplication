b6f14f3640cd5ef6ae958ae5108b1d8321ce1b71
znach <znachok.egor@gmail.com>
2025-09-18 12:07:46 +0300

    add repositories

--- src/main/java/org/example/blog_platform/BlogPlatformApplication.java
+++ src/main/java/org/example/blog_platform/BlogPlatformApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlogPlatformApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BlogPlatformApplication.class, args);
     }
---
+++ src/main/java/org/example/blog_platform/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package org.example.blog_platform.repository;
+
+import org.example.blog_platform.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
